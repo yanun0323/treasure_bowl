6d8a82f6f9ddfc4d4a4f4b65c7d6e773460493d1
yanun0323 <a1256212562@gmail.com>
2023-11-11 14:58:23 +0800

    add infra

--- main.go
+++ main.go
@@ -1,5 +1,14 @@
 package main
 
-func main() {
+import (
+	"log"
+	"main/internal/app"
+	"main/pkg/infra"
+)
 
+func main() {
+	if err := infra.Init("config"); err != nil {
+		log.Fatalf("init infra, err: %+v", err)
+	}
+	app.Run()
 }
---
+++ pkg/infra/infra.go
@@ -0,0 +1,7 @@
+package infra
+
+import "github.com/yanun0323/pkg/config"
+
+func Init(cfgName string) error {
+	return config.Init(cfgName, true, "../config", "../../config", "./config")
+}
