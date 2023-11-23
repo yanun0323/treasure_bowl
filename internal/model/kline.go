f364d8ff65b2b467f4e50a5540b6ce15cb53aa6f
yanun0323 <a1256212562@gmail.com>
2023-11-23 11:47:45 +0800

    add klineType cron spec and rm K3d

--- internal/model/kline.go
+++ internal/model/kline.go
@@ -17,10 +17,44 @@ const (
 	K8h
 	K12h
 	K1d
-	K3d
 	K1w
 	K1M
 )
+
+func (t KlineType) CronSpec() string {
+	switch t {
+	case K1m:
+		return "0 * * * * *"
+	case K3m:
+		return "0 */3 * * * *"
+	case K5m:
+		return "0 */5 * * * *"
+	case K15m:
+		return "0 */15 * * * *"
+	case K30m:
+		return "0 */30 * * * *"
+	case K1h:
+		return "0 0 * * * *"
+	case K2h:
+		return "0 0 */2 * * *"
+	case K4h:
+		return "0 0 */4 * * *"
+	case K6h:
+		return "0 0 */6 * * *"
+	case K8h:
+		return "0 0 */8 * * *"
+	case K12h:
+		return "0 0 */12 * * *"
+	case K1d:
+		return "0 0 0 * * *"
+	case K1w:
+		return "0 0 0 0 0 *"
+	case K1M:
+		return "0 0 0 0 * *"
+
+	}
+	return "0 * * * * *"
+}
 
 type Kline struct {
 	Pair       Pair
